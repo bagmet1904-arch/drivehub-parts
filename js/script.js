87ea4b42852cb27af55ac15c8dbaf0d5c214a6a4
bagmet1904-arch <bagmet.19.04@gmail.com>
2026-07-06 17:03:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,28 @@ function searchProduct() {
 
     window.location.href = "catalog.html?search=" + encodeURIComponent(search);
 }
+window.onload = function () {
+
+    const params = new URLSearchParams(window.location.search);
+
+    const search = params.get("search");
+
+    if (!search) return;
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
+        const name = card.dataset.name;
+
+        if (!name) return;
+
+        if (!name.includes(search.toLowerCase())) {
+
+            card.style.display = "none";
+
+        }
+
+    });
+
+};
