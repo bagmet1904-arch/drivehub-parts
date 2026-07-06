3991a11e11aab3d2c787811893a39cf8e65f5522
bagmet1904-arch <bagmet.19.04@gmail.com>
2026-07-06 16:41:21 +0300

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,17 @@
 // JavaScript сайту
+function searchProduct(){
+
+let text=document
+.getElementById("searchInput")
+.value
+.toLowerCase();
+
+if(text==""){
+alert("Введіть назву товару");
+return;
+}
+
+window.location.href=
+"catalog.html?search="+text;
+
+}
